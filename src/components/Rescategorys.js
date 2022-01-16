b36074c343160659ff9024a054887ddc16dfa77d
omar-ez95 <omar.eddin@gmail.com>
2022-01-16 13:53:13 +0100

    added pagination to main page

--- src/components/Rescategorys.js
+++ src/components/Rescategorys.js
@@ -1,44 +1,74 @@
-import React, {useState, useEffect} from "react"
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import PropTypes from "prop-types";
 import { Row } from "react-bootstrap";
-import Card from "./common/card"
-function Rescategorys(){
+import Card from "./common/card";
+function Rescategorys() {
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [categoryPerPage] = useState(4);
+  const [rescategorys, setRescategorys] = useState([]);
+  const [totalRescategorys, setTotalRescategorys] = useState(0);
+  const [showCategory, setShowCategory] = useState([]);
 
-    const [rescategorys,setRescategorys]=useState([])
-
-    useEffect(() => {
-      axios.get('http://18.192.205.152/api/rescategorys/',
+  const pageNumbers = Math.ceil(totalRescategorys / categoryPerPage);
+  useEffect(() => {
+    axios
+      .get(
+        "http://18.192.205.152/api/rescategorys/",
         { crossDomain: true },
-        { withCredentials: true })
-        .then(response => {
-            setRescategorys( response.data)
+        { withCredentials: true }
+      )
+      .then((response) => {
+        setRescategorys(response.data);
+        setTotalRescategorys(response.data.length);
       });
+  }, []);
 
-  }, [])
+  const handleScroll = (event) => {
+    const { scrollTop, clientHeight, scrollHeight } = event.currentTarget;
+    if(currentPage +1 > pageNumbers)return
+    if (scrollHeight - scrollTop === clientHeight) {
+      setCurrentPage((prev) => prev + 1);
+    }
+  };
 
-    const cards = rescategorys.map(
-        rescategory => (     
-            <Card key={rescategory.id} item={rescategory} link={`/rescategory/${rescategory.name}`} />    
-        ))
+  useEffect(() => {
+    const loadCategorys = async () => {
+      setLoading(true);
+      const indexOfLastCategory = currentPage * categoryPerPage;
+      const indexOfFirstCategory = indexOfLastCategory - categoryPerPage;
+      const currentCategorys = rescategorys.slice(indexOfFirstCategory, indexOfLastCategory);
+      setShowCategory((prev) => [...prev, ...currentCategorys]);
+      setLoading(false);
+    };
+    loadCategorys();
+  }, [currentPage,rescategorys]);
 
-    return(
-        <div>
-                <section>
-                    <div className="container-fluid">
-                        <h1 className="page-name">
-                            MAIN
-                        </h1>
-                        <Row>
-                            {cards}
-                        </Row>
-                    </div>
-                </section>
-            </div>
-    )
+  const cards = showCategory.map((rescategory) => (
+    <Card
+      key={rescategory.id}
+      item={rescategory}
+      link={`/rescategory/${rescategory.name}`}
+    />
+  ));
+
+  return (
+    <div>
+      <section>
+        <div className="container-fluid" onScroll={handleScroll}>
+          <h1 className="page-name">MAIN</h1>
+          <Row >
+            {cards}
+          </Row>
+          {loading && <h1>Loading ...</h1>}
+        </div>
+      </section>
+    </div>
+  );
 }
 
-Rescategorys.propTypes={
-    rescategorys: PropTypes.array
-}
-export default Rescategorys
+Rescategorys.propTypes = {
+  rescategorys: PropTypes.array,
+};
+export default Rescategorys;
